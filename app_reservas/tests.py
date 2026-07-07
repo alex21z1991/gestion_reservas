c006d5ecd0f346daff424dab25f8447dcd93ef5b
Franckotan <137749130+Franckotan@users.noreply.github.com>
2026-07-06 21:46:07 -0400

    Revert "Clicky go   bd"

--- app_reservas/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 01a7884da2219d02dfce469eaf997ceb03cd9fed
